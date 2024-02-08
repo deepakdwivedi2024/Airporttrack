e06c9d4480ba1d1d53628356e438444088febcfe
deepakdwivedi2024 <158248105+deepakdwivedi2024@users.noreply.github.com>
2024-02-08 13:40:31 +0530

    Update AirportRepository.cs
    Update repositories

--- Repository/AirportRepository.cs
+++ Repository/AirportRepository.cs
@@ -7,14 +7,46 @@ public class AirportRepository : IAirportRepository
         _collection = database.GetCollection<Airport>("Airports");
     }
 
-    public async Task<IEnumerable<Airport>> GetAllAsync()
-    {
-        return await _collection.Find(_ => true).ToListAsync();
-    }
-
     public async Task<IEnumerable<BsonDocument>> AggregateAsync(BsonDocument[] pipeline)
     {
         return await _collection.AggregateAsync<BsonDocument>(pipeline).ToListAsync();
+    }
+
+    public async Task BulkInsertAsync(IEnumerable<Airport> airports)
+    {
+        await _collection.InsertManyAsync(airports);
+    }
+
+    public async Task<IEnumerable<Airport>> GetSortedAndLimitedAsync(int limit)
+    {
+        return await _collection.Find(_ => true).Limit(limit).SortBy(a => a.Name).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Airport>> GetFilteredAsync(string country)
+    {
+        return await _collection.Find(a => a.Country == country).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Airport>> GetNearbyAirportsAsync(double latitude, double longitude, double distance)
+    {
+        var filter = Builders<Airport>.Filter.NearSphere(a => a.Location, new GeoJsonPoint(GeoJsonGeographicCoordinates.From(latitude, longitude)), distance);
+        return await _collection.Find(filter).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Airport>> SearchAsync(string searchText)
+    {
+        var filter = Builders<Airport>.Filter.Text(searchText);
+        return await _collection.Find(filter).ToListAsync();
+    }
+
+    public async Task GenerateCSVAsync(IEnumerable<Airport> airports, string filePath)
+    {
+        // Logic to generate CSV file
+    }
+
+    public async Task UploadToAzureBlobAsync(string filePath, string containerName, string blobName)
+    {
+        // Logic to upload file to Azure Blob Storage
     }
 
     // Implement other CRUD methods
