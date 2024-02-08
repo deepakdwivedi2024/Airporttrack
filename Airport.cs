86497834d24cb573bab583d59dc3c15ad9ba788d
deepakdwivedi2024 <158248105+deepakdwivedi2024@users.noreply.github.com>
2024-02-08 13:21:08 +0530

    Create Airport.cs
    Airport model

---
+++ Airport.cs
@@ -0,0 +1,17 @@
+public class Airport
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+    public string IATA { get; set; }
+    public string ICAO { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public int Altitude { get; set; }
+    public int Timezone { get; set; }
+    public string DST { get; set; }
+    public string TimezoneTz { get; set; }
+    public string Type { get; set; }
+    public string Source { get; set; }
+}
